571bcdf0f0c6145beda0832dca644560c30aae67
CR-Aparna <aparnacr003@gmail.com>
2025-05-06 21:30:13 +0530

    Add files via upload

---
+++ cycle3/BookDetails.java
@@ -0,0 +1,97 @@
+import java.util.Scanner;
+class Publisher {
+	String publisher;
+	Publisher(String pub) {
+		this.publisher=pub;
+	}
+}
+class Book extends Publisher {
+	String book;
+	String category;
+	Book(String pub,String book,String category) {
+		super(pub);
+		this.book = book;
+		this.category = category;
+	}
+}
+class Literature extends Book {
+	Literature(String pub,String book,String category) {
+		super(pub,book,category);
+	}
+	void display() {
+		System.out.println("Publisher : "+publisher);
+		System.out.println("Book :"+book);
+		System.out.println("Category :"+category);
+	}
+}
+class Fiction extends Book {
+	Fiction(String pub, String book,String category){
+		super(pub,book,category);
+	}
+	void display() {
+		System.out.println("Publisher :"+publisher);
+		System.out.println("Book: "+book);
+		System.out.println("Category :"+category);
+	}
+}
+
+public class BookDetails {
+	public static void main(String[] args){
+		System.out.print("\nEnter the No.of Literature Books");
+		Scanner sc1 = new Scanner(System.in);
+		int num = sc1.nextInt();
+		Literature arr[] = new Literature[num];
+		System.out.println("\n Enter the literature book details\n");
+		int x = 0,j = 0;
+		Scanner sc = new Scanner(System.in);
+		for(int i = 0;i < num;i++){
+			x = i + 1;
+			System.out.println("\n"+x+").");
+			System.out.print("\n Book : ");
+			String book = sc.nextLine();
+			System.out.print("\n Publisher: ");
+			String pub = sc.nextLine();
+			System.out.print("\n Category: ");
+			String category = sc.nextLine();
+
+			arr[i] = new Literature(book,pub,category);
+		}
+		System.out.println("\nEnter the No. of fiction Books");
+		int num1 = sc1.nextInt();
+		Fiction arr1[] = new Fiction[num1];
+		System.out.println("\nEnter the fiction Book Details\n");
+		int x1 = 0,j1 = 0;
+		for(int i = 0;i < num1;i++){
+			x1 = i + 1;
+			System.out.println("\n"+x1+").");
+			System.out.print("\n Book : ");
+			String book = sc.nextLine();
+			System.out.print("\n Publisher: ");
+			String pub = sc.nextLine();
+			System.out.print("\n Category: ");
+			String category = sc.nextLine();
+
+			arr1[i] = new Fiction(book,pub,category);
+		}
+		sc.close();
+		
+		System.out.println("\n*********Informations of all the Literature Books**********");
+		for(int i =0;i<num;i++){
+			j = i +1;
+			System.out.println("\n"+j+").");
+			arr[i].display();
+		}
+		System.out.println("\n*********Informations of all the Fiction Books**********");
+		for(int i =0;i<num1;i++){
+			j1 = i +1;
+			System.out.println("\n"+j1+").");
+			arr1[i].display();
+		}
+	sc1.close();
+}
+}
+
+
+
+
+
