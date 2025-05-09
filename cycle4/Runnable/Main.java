c736426002ce8d516c32580199fe502070c7a5cc
CR-Aparna <aparnacr003@gmail.com>
2025-05-10 00:05:10 +0530

    outputs updated

--- cycle4/Runnable/Main.java
+++ cycle4/Runnable/Main.java
@@ -30,3 +30,33 @@ public class Main {
         
     }
 }
+
+
+
+/*Enter the value of n for Fibonacci series: 15
+Enter the range (start end): 1 20
+even =2
+even =4
+even =6
+even =8
+even =10
+even =12
+even =14
+even =16
+even =18
+even =20
+fibonacci =0
+fibonacci =1
+fibonnoci = 1
+fibonnoci = 2
+fibonnoci = 3
+fibonnoci = 5
+fibonnoci = 8
+fibonnoci = 13
+fibonnoci = 21
+fibonnoci = 34
+fibonnoci = 55
+fibonnoci = 89
+fibonnoci = 144
+fibonnoci = 233
+fibonnoci = 377 */
